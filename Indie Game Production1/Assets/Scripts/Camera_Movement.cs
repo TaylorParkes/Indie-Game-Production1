f1e5025504d749336c00ee96d9150d232511b800
TaylorParkes <75227513+TaylorParkes@users.noreply.github.com>
2023-10-16 16:58:19 +0100

    Started Cannon Code

---
+++ Indie Game Production1/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float sensitivity = 5.0f;
+
+    void Update()
+    {
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+
+
+        //float mouseX = Input.GetAxis("Mouse X");
+        //float mouseY = Input.GetAxis("Mouse Y");
+        //transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity,0);
+    }
+}
---
+++ Indie Game Production1/Assets/Scripts/Launch_Projectile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 3000f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity));
+        }
+    }
+}
