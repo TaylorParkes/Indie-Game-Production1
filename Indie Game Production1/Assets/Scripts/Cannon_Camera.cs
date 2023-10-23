ca91788a24a1c33ce31e2f384ce02a4c1b44349e
TaylorParkes <75227513+TaylorParkes@users.noreply.github.com>
2023-10-23 15:36:43 +0100

    Worked on Camera and Cannon Movement

--- Indie Game Production1/Assets/Scripts/Cannon_Camera.cs
+++ Indie Game Production1/Assets/Scripts/Cannon_Camera.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Movement : MonoBehaviour
+public class Cannon_Camera : MonoBehaviour
 {
     public float sensitivity = 5.0f;
 
     void Update()
     {
-        
+
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity,0);
